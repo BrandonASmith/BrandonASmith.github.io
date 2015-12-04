a66f82ae96bb4e483ac7847715fdfd8afc7b7b8d
Brandon Smith <basmith808@gmail.com>
2015-12-03 17:14:42 -0800

    Adds completed project

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+"use strict";
+
+
+var cell = "brandon",
+	obj1 = "",
+	obj2 = "",
+	turncount = 0;
+	//sounds = ["piano.wav","hihat.wav","harp.wav","clap.wav","shaker.wav","808.wav","drumroll.wav","Yeah.wav","What.wav""piano.wav","hihat.wav","harp.wav","clap.wav","shaker.wav","808.wav","drumroll.wav","Yeah.wav","What.wav"]
+
+
+//var soundPicker = sounds[Math.floor(Math.random() * sounds.length)];
+  //  console.log(soundPicker);
+    //document.write(soundPicker);
+    //document.querySelector("#soundPrint").innerHTML = soundPicker;
+
+
+function play(param) {
+	if (turncount == 0) {
+		function click1(param) {
+			console.log(param)
+			obj1 = param.target.className; //changed Id to className to fix matching problem, but now undefined
+			turncount = 1;
+		}
+		click1(param);
+	} else if (turncount == 1 ) {
+		function click2(param) {
+			console.log(param.target.className)
+			obj2 = param.target.className; //changed Id to className to fix matching problem, but now undefined
+			turncount = 0;
+		}
+		click2(param);
+		checkMatch();
+	}
+}
+
+function checkMatch() {
+	if (obj1 === obj2 ) {
+		console.log("you have a match!")
+	} else {
+		console.log("try again")
+	}
+	obj1 = "";
+	obj2 = "";
+}
+
+$('.cell').on('click', play)
+
+
+
---
+++ assets/js/audio.js
@@ -0,0 +1,187 @@
+
+   //Assigns audio file to each cell + duplicates 
+  //
+ //
+
+   $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/hihat.wav');
+        //audioElement.load()
+        $('#play').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/piano.wav');
+        //audioElement.load()
+        $('#play1').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/harp.wav');
+        //audioElement.load()
+        $('#play2').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/clap.wav');
+        //audioElement.load()
+        $('#play3').click(function() {
+            audioElement.play();
+        });
+    });
+    
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/Shaker.mp3');
+        //audioElement.load()
+        $('#play4').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/808.wav');
+        //audioElement.load()
+        $('#play5').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/drumroll.wav');
+        //audioElement.load()
+        $('#play6').click(function() {
+            audioElement.play();
+        });
+    });  
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/Yeah.wav');
+        //audioElement.load()
+        $('#play7').click(function() {
+            audioElement.play();
+        });
+    });  
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/What.wav');
+        //audioElement.load()
+        $('#play8').click(function() {
+            audioElement.play();
+        });
+    });     
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/gun.mp3');
+        //audioElement.load()
+        $('#play9').click(function() {
+            audioElement.play();
+        });
+    }); 
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/hihat.wav');
+        //audioElement.load()
+        $('#play10').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/piano.wav');
+        //audioElement.load()
+        $('#play11').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/harp.wav');
+        //audioElement.load()
+        $('#play12').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/clap.wav');
+        //audioElement.load()
+        $('#play13').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/Shaker.mp3');
+        //audioElement.load()
+        $('#play14').click(function() {
+            audioElement.play();
+        });
+    });
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/808.wav');
+        //audioElement.load()
+        $('#play15').click(function() {
+            audioElement.play();
+        });
+    });
+    
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/drumroll.wav');
+        //audioElement.load()
+        $('#play16').click(function() {
+            audioElement.play();
+        });
+    });  
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/Yeah.wav');
+        //audioElement.load()
+        $('#play17').click(function() {
+            audioElement.play();
+        });
+    });  
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/What.wav');
+        //audioElement.load()
+        $('#play18').click(function() {
+            audioElement.play();
+        });
+    });        
+    
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', './assets/sounds/gun.mp3');
+        //audioElement.load()
+        $('#play19').click(function() {
+            audioElement.play();
+        });
+    });        
+
+    //console.log($('#play').audio.attr('src'))
+    //console.log($('#play10').audio.attr('src'))
